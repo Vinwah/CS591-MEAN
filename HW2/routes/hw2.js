3850609323049c239b43fb00975581456e030922
Vincent Wahl <vincentwahl@dhcp-wifi-8021x-155-41-16-217.bu.edu>
2017-06-12 12:14:39 -0400

    Commited v.0 HW2

---
+++ HW2/routes/hw2.js
@@ -0,0 +1,108 @@
+let express = require('express');
+let router = express.Router();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cs591');
+const db = mongoose.connection;
+
+
+db.once('open', function () {
+    console.log('Connection successful.')
+});
+
+const Schema = mongoose.Schema;
+const hw2Schema = new Schema({
+    theString: String,
+    theLength: String
+});
+
+
+const stringModel = mongoose.model('string', hw2Schema);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    stringModel.find({}, function (err, results) {
+        if (err) {res.json(err)}
+        else{
+            let parsed = []
+            for (var i = 0; i < results.length; i++) {
+                parsed.push({
+                    string: results[i]._doc.theString,
+                    length: results[i]._doc.theLength
+                })
+            }
+            res.json(parsed)}
+    })
+});
+
+
+
+router.get('/:string', function(req, res, next) {
+
+    stringModel.find({theString: req.params.string}, function (err, results) {
+        if (err) {res.json(err)}
+        else{
+            if (results.length != 0){
+                res.json({string: results[0]._doc.theString, length: results[0]._doc.theLength})
+            }
+            else {
+                let theString = req.params.string;
+                let lenOfString = theString.length;
+                const aString = new stringModel ( {theString: theString, theLength: lenOfString})
+                aString.save(function(err, results) {
+                    if (err) {res.send(err)}
+
+                    else {res.json({string: results._doc.theString, length: results._doc.theLength})}
+                });
+            }
+        }
+    })
+});
+
+router.post('/', function(req, res, next){
+    if (req.body.string && req.body.string != "") {
+        stringModel.find({theString: req.body.string}, function(err, results){
+            if (err) {res.json(err)}
+            else{
+                if (results.length != 0){
+                    res.json({string: results[0]._doc.theString, length: results[0]._doc.theLength})
+                }
+                else {
+                    let theString = req.body.string;
+                    let lenOfString = req.body.string.length;
+                    const aString = new stringModel ( {theString: theString, theLength: lenOfString})
+                    aString.save(function(err, results) {
+                        if (err) {res.send(err)}
+
+                        else {res.json({string: results._doc.theString, length: results._doc.theLength})}
+                    });
+                }
+            }
+        })
+    }else{
+        res.json({error: "When doing post, a string must be provided: key=string, value='input string'"})
+    }
+})
+
+
+router.delete('/:string', function(req, res, next) {
+    stringModel.find({theString: req.params.string}, function (err, results) {
+        if (err) {res.json(err)}
+        else{
+            if (results.length != 0){
+                stringModel.findOneAndRemove({ theString: req.params.string }, function(err) {
+                    if (err) {res.json(err)}
+                    else {
+                        res.json({string: results[0]._doc.theString, length: results[0]._doc.theLength})
+                    }
+                });
+            }
+            else {
+                res.json({error: "String not found"})
+
+            }
+        }
+    })
+});
+
+module.exports = router;
